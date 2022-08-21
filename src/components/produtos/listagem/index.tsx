fa28f642b63b53f33bab5b3646ad3cd798fa7e13
Joao Felipe <jfcbxp@gmail.com>
2022-08-21 11:51:29 -0300

    adicionado funcao de deletar produto

--- src/components/produtos/listagem/index.tsx
+++ src/components/produtos/listagem/index.tsx
@@ -6,8 +6,15 @@ import useSWR from "swr";
 import { httpClient } from "app/http";
 import { AxiosResponse } from "axios";
 import Router from "next/router";
+import { useProdutoService } from "app/service";
+import { Alert } from "components/common/message";
+import { useState, useEffect } from "react";
 
 export const ListagemProdutos: React.FC = () => {
+  const service = useProdutoService();
+  const [messagens, setMessages] = useState<Array<Alert>>([]);
+  const [lista, setLista] = useState<Produto[]>([]);
+
   const { data: result, error } = useSWR<AxiosResponse<Array<Produto>>>(
     "/api/produtos",
     (url) => httpClient.get(url)
@@ -15,15 +22,27 @@ export const ListagemProdutos: React.FC = () => {
 
   const editar = (produto: Produto) => {
     const url = `/cadastros/produtos?id=${produto.id}`;
-    Router.push(url)
+    Router.push(url);
   };
 
   const deletar = (produto: Produto) => {
-    console.log(produto);
+    if (produto.id) {
+      service.deletar(produto.id).then((response) => {
+        setMessages([
+          { tipo: "success", texto: "Produto excluido com sucesso!" },
+        ]);
+        const listaAlterada = lista?.filter((p) => p.id !== produto.id);
+        setLista(listaAlterada);
+      });
+    }
   };
 
+  useEffect(() => {
+    setLista(result?.data || []);
+  }, [result]);
+
   return (
-    <Layout titulo="Produtos">
+    <Layout titulo="Produtos" mensagens={messagens}>
       <Loader show={!result} />
       <Link href="/cadastros/produtos">
         <button className="button is-warning"> Novo</button>
@@ -31,11 +50,7 @@ export const ListagemProdutos: React.FC = () => {
       <br />
       <br />
 
-      <TabelaProdutos
-        onEdit={editar}
-        onDelete={deletar}
-        produtos={result?.data || []}
-      />
+      <TabelaProdutos onEdit={editar} onDelete={deletar} produtos={lista} />
     </Layout>
   );
 };
